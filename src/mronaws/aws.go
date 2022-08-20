b54b00cadc74d5eec05f4c48c6523f8628a5952a
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2022-08-19 20:13:09 -0700

    Added methods and tests for list, delete, and download

--- src/mronaws/aws.go
+++ src/mronaws/aws.go
@@ -13,9 +13,13 @@ import "log"
 import "github.com/aws/aws-sdk-go/aws"
 import "github.com/aws/aws-sdk-go/aws/credentials"
 import "github.com/aws/aws-sdk-go/aws/session"
+import "github.com/aws/aws-sdk-go/service/s3"
 import "github.com/aws/aws-sdk-go/service/s3/s3manager"
 
+var AWS_SESSION *session.Session
+var AWS_S3 *s3.S3
 var AWS_UPLOADER *s3manager.Uploader
+var AWS_DOWNLOADER *s3manager.Downloader
 
 const AWS_S3_REGION string = "us-west-1"
 const AWS_S3_BUCKET_NAME string = "mapreducedata"
@@ -24,7 +28,11 @@ const AWS_S3_CREDENTIALS string = "s3_creds.csv"
 const AWS_S3_ACCESS_KEY_ID_INDEX int = 2
 const AWS_S3_SECRET_KEY_INDEX int = 3
 
-func CreateUploader() *s3manager.Uploader {
+func createSession() *session.Session {
+	if AWS_SESSION != nil {
+		return AWS_SESSION
+	}
+
 	file, _ := os.Open(AWS_S3_CREDENTIALS)
 	r := csv.NewReader(file)
 	var prev_record []string
@@ -36,40 +44,117 @@ func CreateUploader() *s3manager.Uploader {
 		prev_record = record
 	}
 
-	fmt.Println("WHAT IS RECORD", prev_record)
 	s3Config := &aws.Config{
-		Region: 		aws.String(AWS_S3_BUCKET_NAME),
+		Region: 		aws.String(AWS_S3_REGION),
 		Credentials: 	credentials.NewStaticCredentials(prev_record[AWS_S3_ACCESS_KEY_ID_INDEX], prev_record[AWS_S3_SECRET_KEY_INDEX], ""),
 	}
-
-	s3Session := session.New(s3Config)
-	return s3manager.NewUploader(s3Session)
+	return session.New(s3Config)
 }
 
-func AddFileToS3(filename string) (*s3manager.UploadOutput, bool) {
-	if AWS_UPLOADER == nil {
-		AWS_UPLOADER = CreateUploader()
+func createS3() *s3.S3 {
+	if AWS_S3 != nil {
+		return AWS_S3
 	}
 
-	fmt.Println("Uploading...")
+	return s3.New(createSession())
+}
+
+func createUploader() *s3manager.Uploader {
+	if AWS_SESSION == nil {
+		AWS_SESSION = createSession()
+	}
+
+	return s3manager.NewUploader(AWS_SESSION)
+}
+
+func createDownloader() *s3manager.Downloader {
+	if AWS_SESSION == nil {
+		AWS_SESSION = createSession()
+	}
+
+	return s3manager.NewDownloader(AWS_SESSION)
+}
+
+func AddFileToS3(filename string, prefix string) (*s3manager.UploadOutput, error) {
+	if AWS_UPLOADER == nil {
+		AWS_UPLOADER = createUploader()
+	}
 
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	filepath := filename
+	if prefix != "" {
+		filepath = fmt.Sprintf("%s/%s", prefix, filename)
+	}
 	addFileInput := &s3manager.UploadInput{
 		Bucket: 		aws.String(AWS_S3_BUCKET_NAME),
-		Key: 			aws.String(fmt.Sprintf("output/%s", filename)),
+		Key: 			aws.String(filepath),
 		Body: 			bytes.NewReader(file),
 		ContentType: 	aws.String("text"),
 	}
 
 	uploadOutput, err := AWS_UPLOADER.UploadWithContext(context.Background(), addFileInput)
-	if err != nil {
-		return nil, false
+	return uploadOutput, err
+}
+
+func DownloadFileInS3(key string) error {
+	if AWS_DOWNLOADER == nil {
+		AWS_DOWNLOADER = createDownloader()
 	}
-	return uploadOutput, true
+
+	file, err := os.Create(key)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	_, err = AWS_DOWNLOADER.Download(
+		file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(AWS_S3_BUCKET_NAME),
+			Key: aws.String(key),
+		},
+	)
+	return err
+}
+
+func ListFilesInS3(prefix string) (*s3.ListObjectsV2Output, error) {
+	if AWS_S3 == nil {
+		AWS_S3 = createS3()
+	}
+
+	resp, err := AWS_S3.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(AWS_S3_BUCKET_NAME),
+		Prefix: aws.String(prefix),
+	})
+	return resp, err
+}
+
+func DeleteFileInS3(key string) error {
+	if AWS_S3 == nil {
+		AWS_S3 = createS3()
+	}
+
+	_, err := AWS_S3.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(AWS_S3_BUCKET_NAME),
+		Key: aws.String(key),
+	})
+	if err != nil {
+		return err
+	}
+
+	err = AWS_S3.WaitUntilObjectNotExists(&s3.HeadObjectInput{
+		Bucket: aws.String(AWS_S3_BUCKET_NAME),
+		Key: aws.String(key),
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 
--- src/mronaws/aws_test.go
+++ src/mronaws/aws_test.go
@@ -3,7 +3,74 @@ package mronaws
 import "fmt"
 import "testing"
 
-func TestAWSCreateUploader(t *testing.T) {
-	s3Uploader := CreateUploader()
-	fmt.Println(s3Uploader)
+const TEST_OBJECT_PREFIX string = "test"
+const TEST_OBJECT_NAME string = "README_TEST.md"
+var TEST_OBJECT_KEY string = fmt.Sprintf("%s/%s", TEST_OBJECT_PREFIX, TEST_OBJECT_NAME)
+
+func TestAddFileToS3(t *testing.T) {
+	t.Run("AddFileToS3", func(t *testing.T) {
+		_, err := AddFileToS3(TEST_OBJECT_NAME, TEST_OBJECT_PREFIX)
+		if err != nil {
+			t.Errorf("There was an error uploading the file to S3 %v", err)
+		}
+		DeleteFileInS3(TEST_OBJECT_KEY)
+	})
+}
+
+func TestListFilesInS3(t *testing.T) {
+	t.Run("ListFilesInS3", func(t *testing.T) {
+		AddFileToS3(TEST_OBJECT_NAME, TEST_OBJECT_PREFIX)
+		objects, err := ListFilesInS3(TEST_OBJECT_PREFIX)
+		if err != nil {
+			t.Errorf("There was an error listing the files in S3 %v", err)
+		}
+		if len(objects.Contents) != 1 {
+			t.Errorf("Expected one object to exist. %d found", len(objects.Contents))
+		}
+		if *objects.Contents[0].Key != TEST_OBJECT_KEY {
+			t.Errorf("Expected object to be %s but was %s", TEST_OBJECT_KEY, *objects.Contents[0].Key)
+		}
+		DeleteFileInS3(TEST_OBJECT_KEY)
+	})
+}
+
+func TestAddListAndDeleteFileInS3(t *testing.T) {
+	t.Run("DeleteFileInS3", func(t *testing.T) {
+		AddFileToS3(TEST_OBJECT_NAME, TEST_OBJECT_PREFIX)
+		
+		objects, err := ListFilesInS3(TEST_OBJECT_PREFIX)
+		if len(objects.Contents) != 1 {
+			t.Errorf("Expected one object to exist. %d found", len(objects.Contents))
+		}
+		if *objects.Contents[0].Key != TEST_OBJECT_KEY {
+			t.Errorf("Expected object to be %s but was %s", TEST_OBJECT_KEY, *objects.Contents[0].Key)
+		}
+		
+		err = DeleteFileInS3(TEST_OBJECT_KEY)
+		if err != nil {
+			t.Errorf("Error %v during deletion of %s", err, TEST_OBJECT_NAME)
+		}
+		objects, _ = ListFilesInS3(TEST_OBJECT_PREFIX)
+		if len(objects.Contents) != 0 {
+			t.Errorf("Expected no objects to exist. %d found", len(objects.Contents))
+		}
+	})
+}
+
+func TestDownloadFileInS3(t *testing.T) {
+	t.Run("DownloadFileInS3", func(t *testing.T) {
+		removeFiles([]string{TEST_OBJECT_KEY})
+		AddFileToS3(TEST_OBJECT_NAME, TEST_OBJECT_PREFIX)
+
+		err := DownloadFileInS3(TEST_OBJECT_KEY)
+		if err != nil {
+			t.Errorf("Expected no error during download %v", err)
+		}
+		if !exists(TEST_OBJECT_KEY) {
+			t.Errorf("Expected file %s to exist on local filesystem", TEST_OBJECT_KEY)
+		}
+
+		removeFiles([]string{TEST_OBJECT_KEY})
+		DeleteFileInS3(TEST_OBJECT_KEY)
+	})
 }
