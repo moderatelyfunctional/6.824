63d4af5d2d602deae9e8d85e533dae67cdd31b47
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2023-02-16 21:33:38 -0800

    Lab3A: Added success

--- src/kvraft/common.go
+++ src/kvraft/common.go
@@ -10,12 +10,13 @@ type Err string
 
 // Put or Append
 type PutAppendArgs struct {
-	Key   string
-	Value string
-	Op    string // "Put" or "Append"
+	Key   		string
+	Value 		string
+	Op    		string // "Put" or "Append"
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Sucess 		bool
 }
 
 type PutAppendReply struct {
